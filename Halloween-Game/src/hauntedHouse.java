fa9131a79d9be183e86f329e240c452d7cf27e04
Esai Flores <flores.esaijonathan@gmail.com>
2020-07-31 21:33:00 -0500

    setting up halloween adventure game

---
+++ Halloween-Game/src/hauntedHouse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class hauntedHouse {
+    Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        hauntedHouse game = new hauntedHouse();
+        game.intro();
+        game.startGame();
+    }
+
+    public void intro () {
+        System.out.println("\n"
+                );
+    }
+
+    public void startGame (){
+
+    }
+
+
+}
